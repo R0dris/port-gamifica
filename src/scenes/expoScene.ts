4ab9ff62cb8afe2de4ff58c977a845b98c4e31a8
João Vitor <joaovitorrods089@gmail.com>
2024-06-12 17:20:02 -0300

    feat: Colisões Iniciadas

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,4 @@
-import { Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
 import { Player } from "../actors/player";
 
@@ -30,10 +30,33 @@ export class expoScene extends Scene{
         // Criação e configuração do player
         let jogador = new Player()
 
-        
-        
+        jogador.z = 2
+                
         // Adicionar o player na cena
         this.add(jogador)
 
+        // Adicionar colisão com cada objeto
+        // Pegar a camada de objetos colisores
+        let camadaObjetosColisores = tiledMap.getObjectLayers("Objetos colisores")[0]
+
+        console.log(camadaObjetosColisores);
+
+        // Percorrer objetos com for each e para cada objeto, renderizar um actor
+        camadaObjetosColisores.objects.forEach(objeto => {
+            const objetoAtual = new Actor({
+                name: objeto.name,
+                x: objeto.x + offsetx + (objeto.tiledObject.width! / 2),
+                y: objeto.y + offsetY + (objeto.tiledObject.height! / 2),
+                width: objeto.tiledObject.width,
+                height: objeto.tiledObject.height,
+                collisionType: CollisionType.Fixed
+
+            })
+
+            // Adicionar o colisor do objeto na cena
+            this.add(objetoAtual)
+        })
+
+        
     }
 }
